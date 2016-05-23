65888f5d3515942fda518ee353252d0cc087c598
Arne Fahrenwalde <macgeneral@macgeneral.de>
2016-05-23 22:35:15 +0200

    changed author to team name

--- pyrunner_classes/__init__.py
+++ pyrunner_classes/__init__.py
@@ -3,6 +3,5 @@ from .render_thread import RenderThread
 from .sound_thread import MusicMixer
 
 __version__ = '0.1.0'
-__author__ = 'Arne Fahrenwalde'
-__author_email__ = 'arne@fahrenwal.de'
+__author__ = 'Team pyBerries'
 __description__ = 'Sub Classes of the pyRunner Game'
