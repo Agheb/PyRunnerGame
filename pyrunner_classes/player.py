c970e76224063165008a38db880d98e86f162d8b
Arne Fahrenwalde <macgeneral@macgeneral.de>
2016-06-22 22:19:57 +0200

    cleaned up player animation creation

--- pyrunner_classes/player.py
+++ pyrunner_classes/player.py
@@ -28,6 +28,7 @@ class Player(pygame.sprite.DirtySprite):
         self.change_y = 0
         self.speed = 4
         self.gold_count = 0
+        self.tile_size = 32
 
         # list holding the image for movement. Up and down movement uses the same sprites.
         self.walking_frames_l = []
@@ -37,97 +38,49 @@ class Player(pygame.sprite.DirtySprite):
         self.digging_frames_r = []
         self.hanging_frames_l = []
         self.hanging_frames_r = []
-
-        sprite_sheet = SpriteSheet(SPRITE_SHEET_PATH)
+        self.sprite_sheet = SpriteSheet(SPRITE_SHEET_PATH)
 
         # Load all the left facing images into a list (x, y, height, width)
-        image = sprite_sheet.get_image(96, 0, 32, 32)
-        self.walking_frames_l.append(image)
-        image = sprite_sheet.get_image(64, 0, 32, 32)
-        self.walking_frames_l.append(image)
-        image = sprite_sheet.get_image(32, 0, 32, 32)
-        self.walking_frames_l.append(image)
-        image = sprite_sheet.get_image(0, 0, 32, 32)
-        self.walking_frames_l.append(image)
-
+        self.add_animation(0, 0, self.walking_frames_l, 4)
         # Load all the left facing images into a list and flip them to make them face right
-        image = sprite_sheet.get_image(96, 0, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_r.append(image)
-        image = sprite_sheet.get_image(64, 0, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_r.append(image)
-        image = sprite_sheet.get_image(32, 0, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_r.append(image)
-        image = sprite_sheet.get_image(0, 0, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.walking_frames_r.append(image)
-
+        self.add_animation(0, 0, self.walking_frames_r, 4, True)
         # Load all the up / down facing images into a list
-        image = sprite_sheet.get_image(96, 32, 32, 32)
-        self.walking_frames_ud.append(image)
-        image = sprite_sheet.get_image(64, 32, 32, 32)
-        self.walking_frames_ud.append(image)
-        image = sprite_sheet.get_image(32, 32, 32, 32)
-        self.walking_frames_ud.append(image)
-        image = sprite_sheet.get_image(0, 32, 32, 32)
-        self.walking_frames_ud.append(image)
-
+        self.add_animation(0, 1, self.walking_frames_ud, 4)
         # Load all the digging left images
-        image = sprite_sheet.get_image(64, 64, 32, 32)
-        self.digging_frames_l.append(image)
-        image = sprite_sheet.get_image(32, 64, 32, 32)
-        self.digging_frames_l.append(image)
-        image = sprite_sheet.get_image(0, 64, 32, 32)
-        self.digging_frames_l.append(image)
-        image = sprite_sheet.get_image(16, 64, 32, 32)
-        self.digging_frames_l.append(image)
-
+        self.add_animation(0, 2, self.digging_frames_l, 3)
         # Load all the digging left images and flip them do digging right
-        image = sprite_sheet.get_image(64, 64, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.digging_frames_r.append(image)
-        image = sprite_sheet.get_image(32, 64, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.digging_frames_r.append(image)
-        image = sprite_sheet.get_image(0, 64, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.digging_frames_r.append(image)
-
+        self.add_animation(0, 2, self.digging_frames_r, 3, True)
         # Load the left hanging images into a list
-        image = sprite_sheet.get_image(128, 32, 32, 32)
-        self.hanging_frames_l.append(image)
-        image = sprite_sheet.get_image(160, 32, 32, 32)
-        self.hanging_frames_l.append(image)
-        image = sprite_sheet.get_image(192, 32, 32, 32)
-        self.hanging_frames_l.append(image)
-        image = sprite_sheet.get_image(224, 32, 32, 32)
-        self.hanging_frames_l.append(image)
-
+        self.add_animation(5, 1, self.hanging_frames_l, 4)
         # Load the left hanging images into a list and flip them to face right
-        image = sprite_sheet.get_image(128, 32, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.hanging_frames_r.append(image)
-        image = sprite_sheet.get_image(160, 32, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.hanging_frames_r.append(image)
-        image = sprite_sheet.get_image(192, 32, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.hanging_frames_r.append(image)
-        image = sprite_sheet.get_image(224, 32, 32, 32)
-        image = pygame.transform.flip(image, True, False)
-        self.hanging_frames_r.append(image)
+        self.add_animation(5, 1, self.hanging_frames_r, 4, True)
 
         # Stop Frame: Sprite when player is not moving on ground
-        self.stop_frame = sprite_sheet.get_image(160, 0, 32, 32)
+        self.stop_frame = self.add_animation(5, 0)
 
         self.direction = "Stop"  # direction the player is facing at the beginning of the game
 
         # Set the image the player starts with
-        self.image = self.walking_frames_r[0]
+        self.image = self.stop_frame
         # Set a reference to the image rect.
         self.rect = self.image.get_rect()
+
+    def add_animation(self, pos_x, pos_y, list=None, frames=1, flip=False):
+        ts = self.tile_size
+        pos_y *= ts
+
+        try:
+            for i in range(frames):
+                image = self.sprite_sheet.get_image(pos_x, pos_y, ts, ts)
+                if flip:
+                    image = pygame.transform.flip(image, True, False)
+                list.append(image)
+                pos_x += ts
+
+            return list
+        except AttributeError:
+            return self.sprite_sheet.get_image(pos_x * ts, pos_y * ts, ts, ts)
+
 
     # Player-controlled movement:
     def go_left(self):
